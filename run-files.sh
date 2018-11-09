279d2e2714c04cce8053fc67a0b082e716d47455
Bob Zacher <bzacher100@gmail.com>
2018-11-08 21:30:09 -0500

    added run-files.sh

--- run-files.sh
+++ run-files.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #FILES=/path/to/*
-FILES=~/Eng/brandsure/10.Commission/*.xml
+FILES=`find /home/bob/Eng/brandsure/test -name \*.xml `
+#FILES=~/Eng/brandsure/10.Commission/*.xml
+
 
 for f in $FILES
 do
   echo "Processing $f file..."
   # take action on each file. $f store current file name
-  f-fixed=$f+"-fixed"
+  ./run-filemod.sh $f output
   echo "Processing $f-fixed file..."
-  #sed -i '/^[[:space:]]*$/d'
+  sed -i '/^[[:space:]]*$/d'  $f-fixed
 done
